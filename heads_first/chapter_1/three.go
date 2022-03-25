d6940fe317808ab72423f651264c1021ce3e4023
luchanos <Forgotten1000>
2022-03-26 02:39:14 +0400

    [upd] test go

---
+++ heads_first/chapter_1/three.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	price := 100
+	fmt.Println("Price is", price, "dollars")
+
+	var taxRate float64 = 0.08
+	tax := float64(price) * taxRate
+	fmt.Println("Tax is", tax, "dollars")
+
+	total := float64(price) + tax
+	fmt.Println("Total cost is", total, "dollars")
+
+	availableFunds := 120
+	fmt.Println(availableFunds, "dollars available.")
+	fmt.Println("Within budget?", total <= float64(availableFunds))
+}
