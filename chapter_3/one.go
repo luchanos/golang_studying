f9eeb376d71126157ae3c724ed7b5ca87d339d31
luchanos <Forgotten1000>
2022-04-13 00:36:42 +0400

    [upd] test go

--- chapter_3/one.go
+++ chapter_3/one.go
@@ -1,17 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 func paintNeeded(height float64, width float64) float64 {
+	if height < 0 || width < 0 {
+		err := errors.New("Value can't be negative!")
+		log.Fatal(err)
+	}
 	area := width * height
 	return area / 10
 }
 
 func main() {
 	var total, amount float64
-	amount = paintNeeded(4.2, 3.0)
-	total += amount
-	amount = paintNeeded(5.2, 3.5)
+	amount = paintNeeded(4.2, -3.0)
 	total += amount
 	fmt.Printf("%.2f liters needed\n", total)
 }
